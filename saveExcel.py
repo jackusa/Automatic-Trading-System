ee9043df1258a7274c0ad4056ce0b1228f50808d
unknown <liuxin0123@foxmail.com>
2017-05-24 23:15:03 +0800

    Make saveExcel work

--- saveExcel.py
+++ saveExcel.py
@@ -9,12 +9,15 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 class wrExcel():
-    def __init__(self):
-        self.code = []
-        self.date = []
+    def __init__(self, code, date):
+        
+        self.code = code
+        self.date = date
+        self.filename = code + '#' + date + '.xls'
 
-        self.excel = []
-        self.sheet = []
+        self.excel = xlwt.Workbook()
+        self.sheet = self.excel.add_sheet(self.code)
+
 
         self.initString = ['Name', 'Open', 'CloseYesterday', 'Close', 'Highest', 'Lowest', 'Buy1st', 'Sell1st', 'Volume', 'Amount', 
         'Buy1stVol', 'Buy1st', 'Buy2ndVol', 'Buy2nd', 'Buy3rdVol', 'Buy3rd', 'Buy4thVol', 'Buy4th', 'Buy5thVol', 'Buy5th', 
@@ -25,38 +28,30 @@ class wrExcel():
         self.style1 = xlwt.easyxf('font: name Times New Roman, color-index black') 
         self.style2 = xlwt.easyxf(num_format_str='D-MMM-YY')
 
-    def write(self, code, date, row, data):
-        self.code = code
-        self.date = date
 
-        self.excel = xlwt.Workbook()
-        self.sheet = self.excel.add_sheet(self.code)
-
+    
+    def set(self):
         for i in range (len(self.initString)):
             self.sheet.write(0, i, self.initString[i], self.style0)
 
-        
-
+    def write(self, row, data):
         for i in range (len(data)):
             self.sheet.write(row, i, data[i], self.style1)
 
-        self.excel.save(self.code + '#' + self.date + '.xls')
+    def save(self):    
+        self.excel.save(self.filename)
 
 
-# excel().write('23423', '23421431', 2, [2342, 37657, 67986, 4545])
+    def read(self):
+        print self.code
+        # book = xlrd.open_workbook(self.filename)
 
-#         self.sheet.write(1, 14, datetime.now(), style1)
-#         self.sheet.write(2, 15, 1)
-#         self.sheet.write(2, 1, 1)
+        # print("The number of worksheets is {0}".format(book.nsheets))
+        # print("Worksheet name(s): {0}".format(book.sheet_names()))
+        # sh = book.sheet_by_index(0)
+        # print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
+        # print("Cell D30 is {0}".format(sh.cell_value(rowx=29, colx=3)))
+        # for rx in range(sh.nrows):
+        #     print(sh.row(rx))
+
 #         self.sheet.write(2, 2, xlwt.Formula("A3+B3"))
-
-# book = xlrd.open_workbook("myfile.xls")
-# print("The number of worksheets is {0}".format(book.nsheets))
-# print("Worksheet name(s): {0}".format(book.sheet_names()))
-# sh = book.sheet_by_index(0)
-# print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
-# print("Cell D30 is {0}".format(sh.cell_value(rowx=29, colx=3)))
-# for rx in range(sh.nrows):
-#     print(sh.row(rx))
-
-
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
  # -*- coding: utf-8 -*-
 import sys
-import time 
+import time
+from datetime import datetime 
 
 from requestGet import requestGet
 from url import getURL
@@ -18,10 +19,14 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 code = '002407'
+myExcel = wrExcel(code, str(datetime.now().strftime("%y-%m-%d")))
+myExcel.set()
+
+cnt  = 1 # row number of excel 
 
 while (not workSchedule().ifEnd()):
-    cnt  = 1
-    if not workSchedule().ifWork():
+    
+    if workSchedule().ifWork():
         
         if workSchedule().ifSample():
             dataStr = requestGet(getURL(code))
@@ -31,17 +36,19 @@ while (not workSchedule().ifEnd()):
             dateStr = dataList[-3]
             timeStr = dataList[-2]
             saveTXT(code, dateStr, timeStr, dataStr)
-            wrExcel().write(code, dateStr, cnt, dataList)
+            myExcel.write(cnt, dataList)
             cnt  = cnt + 1
 
             price = str2data(dataList)
             
             print cnt
-
+            time.sleep(1) 
+            # The code began to run at XX:XX:00. But sometimes its work can be finished in 1 second. 
+            # So I add a dealy time to avoid running code with multiple cycles in 1 second.
         else:
             pass
    
     else:
         pass
 
-
+myExcel.save()
