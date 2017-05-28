f9c864c0ad95cef8f09f87df6b699904229cf5cb
LIUXIN <liuxin.bit@gmail.com>
2017-05-27 23:18:01 -0400

    little change

--- thred.py
+++ thred.py
@@ -1,2 +1,10 @@
 import os
 
+print  'Process (%s) start...' % (os.getpid())
+pid = os.fork()
+
+if pid ==0:
+	print 'child Process (%s) and parent process (%s)' % (os.getpid(), os.getppid())
+else:
+	print 'The %s just create a child process(%s)' % (os.getpid(), pid)
+
